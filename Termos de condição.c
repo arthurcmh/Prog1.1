590dbba80059377a2a5c6581a08eb472a465d288
Tocolinus <32079282+Tocolinus@users.noreply.github.com>
2017-10-04 18:03:48 -0300

    Add files via upload

---
+++ Termos de condição.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+int num;
+
+printf("Escolha um numero de 1 a 4\n");
+scanf("%d", &num);
+
+if (num == 1){printf("\nEsse numero e 1\n");}
+
+else if (num == 2){printf("\nEsse numero e 2\n");}
+
+else if (num == 3){printf("\nEsse numero e 3\n");}
+
+else if (num == 4){printf("\nEsse numero e 4\n");};
+
+system("pause");
+}
